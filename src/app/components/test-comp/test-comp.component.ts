c8a7d7fc4088f9b097cce585a86c6061e4dbace4
Erwin-Feld <erwinfeld.dev@gmail.com>
2022-03-26 12:54:33 +0100

    added for loop to display values

--- src/app/components/test-comp/test-comp.component.ts
+++ src/app/components/test-comp/test-comp.component.ts
@@ -3,12 +3,23 @@ import { Apollo } from 'apollo-angular';
 
 import { FIRST_QUERY } from 'src/app/graphql/graphql.queries';
 
+// {__typename: 'User', id: '1', firstname: 'Mike', age: 12}
+
+export interface Users {
+  __typename: string,
+  id: string,
+  firstname: string,
+  age: number
+}
+
 @Component({
   selector: 'app-test-comp',
   templateUrl: './test-comp.component.html',
   styleUrls: ['./test-comp.component.scss']
 })
 export class TestCompComponent implements OnInit {
+
+  userData! :Users[]
 
   constructor(private apollo: Apollo) { }
 
@@ -17,6 +28,7 @@ export class TestCompComponent implements OnInit {
       query: FIRST_QUERY,
     }).valueChanges.subscribe((result:any) =>{
       console.log(result?.data?.users)
+      this.userData = result?.data?.users
     })
   }
 
