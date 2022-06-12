c34e0e9377751e8536bc1359a0e19b30ff9bc821
Erwin-Feld <erwinfeld.dev@gmail.com>
2022-06-12 09:38:38 +0200

    changed mock api previos api does not work

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = 'https://fakeql.com/graphql/15298ee2268195df645d656f780a2bea'; // <-- add the URL of the GraphQL server here
+const uri = 'https://api.mocki.io/v2/c4d7a195/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
